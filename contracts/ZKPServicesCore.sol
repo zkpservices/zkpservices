604c34309a70b1df7f982ecdb44a3691513c9812
zkpservices <contact@zkp.services>
2023-11-13 21:54:28 +1100

    feat: CCIP functionality template

--- contracts/ZKPServicesCore.sol
+++ contracts/ZKPServicesCore.sol
@@ -1,9 +1,29 @@
-pragma solidity ^0.8.7;
+pragma solidity 0.8.19;
 
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/ITwoFactor.sol";
 import "./interfaces/IGroth16VerifierP2.sol";
+
+/*
+
+           /$$                                                             /$$                              
+          | $$                                                            |__/                              
+ /$$$$$$$$| $$   /$$  /$$$$$$      /$$$$$$$  /$$$$$$   /$$$$$$  /$$    /$$ /$$  /$$$$$$$  /$$$$$$   /$$$$$$$
+|____ /$$/| $$  /$$/ /$$__  $$    /$$_____/ /$$__  $$ /$$__  $$|  $$  /$$/| $$ /$$_____/ /$$__  $$ /$$_____/
+   /$$$$/ | $$$$$$/ | $$  \ $$   |  $$$$$$ | $$$$$$$$| $$  \__/ \  $$/$$/ | $$| $$      | $$$$$$$$|  $$$$$$ 
+  /$$__/  | $$_  $$ | $$  | $$    \____  $$| $$_____/| $$        \  $$$/  | $$| $$      | $$_____/ \____  $$
+ /$$$$$$$$| $$ \  $$| $$$$$$$//$$ /$$$$$$$/|  $$$$$$$| $$         \  $/   | $$|  $$$$$$$|  $$$$$$$ /$$$$$$$/
+|________/|__/  \__/| $$____/|__/|_______/  \_______/|__/          \_/    |__/ \_______/ \_______/|_______/ 
+                    | $$                                                                                    
+                    | $$                                                                                    
+                    |__/                                                                                    
+                                                    
+*/
 
 contract ZKPServicesCore is ERC20Burnable, Ownable {
     uint256 public requestFee;
@@ -13,14 +33,8 @@ contract ZKPServicesCore is ERC20Burnable, Ownable {
         string key;                         // RSA Public Key
     }
 
-    struct Signature {
-        RSAKey publicKey;                   // RSA Public Key
-        string signedHash;                  // SHA256 signed with RSA private key of updating party
-    }
-
     struct Data {
         string dataHash;                    // SHA256 of associated data
-        Signature signature;                
     }
 
     struct DataRequest {
@@ -40,7 +54,6 @@ contract ZKPServicesCore is ERC20Burnable, Ownable {
         address requester;
         uint256 responseFeeAmount; 
         string newHash;
-        Signature signature;
     }
 
     struct Response {
@@ -48,26 +61,33 @@ contract ZKPServicesCore is ERC20Burnable, Ownable {
     }
 
     /* 
-     CCIP | Cross-Chain Compatible Variables:
+    
+    CCIP | Cross-Chain Compatible Variables:
+        
         rsaKeys
         obfuscatedData
         responses
 
     Partially Compatible Variables:
+        
         dataRequests
         updateRequests
 
-            due to the fact that selected 2FA providers would 
+           *due to the fact that selected 2FA providers would 
             need to be able to support equivalent logic and 
             addresses on the destination chain (this is 
             available with ZKPServicesVRF2FA but may not
             be for others)
+           *to clarify - this is supported via CCIP but not
+            guaranteed to work on other chains as it is 2FA-
+            provider dependent
 
     Incompatible Variables:
+        
         _2FAProviders
         _2FAProviderOwners
 
-            these are either redundant or incompatible to 
+           *these are either redundant or incompatible to 
             sync across chains as the 2FA providers in 
             question would need to be able to support
             equivalent logic and addresses across chains,
@@ -133,7 +153,9 @@ contract ZKPServicesCore is ERC20Burnable, Ownable {
         require(!usedRequestIds[requestId], "This requestId has already been used.");
         require(balanceOf(msg.sender) >= requestFee, "Not enough ZKP tokens for request fee.");
         _burn(msg.sender, requestFee);
-        dataRequests[requestId] = DataRequest(encryptedRequest, encryptedKey, block.timestamp + timeLimit, _2FAProvider, msg.sender, responseFeeAmount);
+        dataRequests[requestId] = DataRequest(encryptedRequest, encryptedKey, 
+                                                block.timestamp + timeLimit, _2FAProvider, 
+                                                msg.sender, responseFeeAmount);
     }
 
     function requestUpdate(
@@ -143,13 +165,14 @@ contract ZKPServicesCore is ERC20Burnable, Ownable {
         uint256 timeLimit,
         address _2FAProvider,
         uint256 responseFeeAmount,
-        string memory newHash,
-        Signature memory signature
+        string memory newHash
     ) external {
         require(!usedRequestIds[requestId], "This requestId has already been used.");
         require(balanceOf(msg.sender) >= requestFee, "Not enough ZKP tokens for request fee.");
         _burn(msg.sender, requestFee);
-        updateRequests[requestId] = UpdateRequest(encryptedRequest, encryptedKey, block.timestamp + timeLimit, _2FAProvider, msg.sender, responseFeeAmount, newHash, signature);
+        updateRequests[requestId] = UpdateRequest(encryptedRequest, encryptedKey, 
+                                                    block.timestamp + timeLimit, _2FAProvider, 
+                                                    msg.sender, responseFeeAmount, newHash);
     }
 
     function respond(
@@ -164,15 +187,19 @@ contract ZKPServicesCore is ERC20Burnable, Ownable {
     ) external {
         require(!usedResponseIds[requestId], "This responseId has already been used.");
         
-        uint256 requiredFee = isUpdate ? updateRequests[requestId].responseFeeAmount : dataRequests[requestId].responseFeeAmount; 
+        uint256 requiredFee = isUpdate ? updateRequests[requestId].responseFeeAmount : 
+                                            dataRequests[requestId].responseFeeAmount; 
         require(balanceOf(msg.sender) >= requiredFee, "Not enough ZKP tokens for response fee.");
 
-        uint256 requestTimeLimit = isUpdate ? updateRequests[requestId].timeLimit : dataRequests[requestId].timeLimit;
+        uint256 requestTimeLimit = isUpdate ? updateRequests[requestId].timeLimit : 
+                                                dataRequests[requestId].timeLimit;
         require(requestTimeLimit >= block.timestamp, "Request has expired.");
  
-        address _2FAProvider = isUpdate ? updateRequests[requestId]._2FAProvider : dataRequests[requestId]._2FAProvider;
+        address _2FAProvider = isUpdate ? updateRequests[requestId]._2FAProvider : 
+                                            dataRequests[requestId]._2FAProvider;
         if (_2FAProvider != address(0)) {
-            ITwoFactor.TwoFactorData memory twoFactorData = ITwoFactor(_2FAProvider).twoFactorData(twoFactorId);
+            ITwoFactor.TwoFactorData memory twoFactorData = 
+                ITwoFactor(_2FAProvider).twoFactorData(twoFactorId);
             require(twoFactorData.success, "2FA failed.");
         }
 
@@ -184,19 +211,106 @@ contract ZKPServicesCore is ERC20Burnable, Ownable {
         );
 
         require(
-            keccak256(bytes(obfuscatedData[dataLocation].dataHash)) == keccak256(bytes(string(abi.encodePacked(_pubSignals[1])))),
+            keccak256(bytes(obfuscatedData[dataLocation].dataHash)) == 
+            keccak256(bytes(string(abi.encodePacked(_pubSignals[1])))),
             "Mismatched data hash."
         );
 
         if (isUpdate) {
             obfuscatedData[dataLocation].dataHash = updateRequests[requestId].newHash;
-            obfuscatedData[dataLocation].signature = updateRequests[requestId].signature;
         }
 
-        address requester = isUpdate ? updateRequests[requestId].requester : dataRequests[requestId].requester;  
+        address requester = isUpdate ? updateRequests[requestId].requester : 
+                                        dataRequests[requestId].requester;  
         _transfer(msg.sender, requester, requiredFee);
 
         usedResponseIds[requestId] = true;
         responses[requestId] = Response({dataLocation: dataLocation});  
+    }
+
+/*
+
+      /$$$$$$   /$$$$$$  /$$$$$$ /$$$$$$$ 
+     /$$__  $$ /$$__  $$|_  $$_/| $$__  $$
+    | $$  \__/| $$  \__/  | $$  | $$  \ $$
+    | $$      | $$        | $$  | $$$$$$$/
+    | $$      | $$        | $$  | $$____/ 
+    | $$    $$| $$    $$  | $$  | $$      
+    |  $$$$$$/|  $$$$$$/ /$$$$$$| $$      
+     \______/  \______/ |______/|__/      
+                                        
+*/                                     
+                                      
+    IRouterClient public senderRouter;
+    LinkTokenInterface public senderToken;
+    mapping(string => CCIPReceiver) public receivers;
+    mapping(string => uint64) public receiverDestinations;
+
+    function setSender(address routerAddress, address linkAddress) 
+    external onlyOwner {
+        senderRouter = IRouterClient(routerAddress);
+        senderToken = LinkTokenInterface(linkAddress);
+    }
+
+    function addReceiver(string calldata receiver, address routerReceiverAddress, uint64 destinationChain) 
+    external onlyOwner {
+        receivers[receiver] = CCIPReceiver(routerReceiverAddress);
+        receiverDestinations[receiver] = destinationChain; 
+    }
+
+    function sendMessage(
+        string memory receiver,
+        string calldata text
+    ) external onlyOwner returns (bytes32 messageId) {
+        require(senderRouter != IRouterClient(address(0)), "Sender not registered");
+        require(receivers[receiver] != CCIPReceiver(address(0)), "Receiver not registered");
+
+        uint64 destinationChainSelector = receiverDestinations[receiver]; 
+        require(destinationChainSelector != 0, "Destination chain not set for receiver");
+
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(receivers[receiver]),
+            data: abi.encode(text),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 200_000, strict: false})
+            ),
+            feeToken: address(senderToken)
+        });
+
+        uint256 fees = senderRouter.getFee(destinationChainSelector, evm2AnyMessage);
+        require(fees <= senderToken.balanceOf(address(this)), "Insufficient LINK balance");
+
+        senderToken.approve(address(senderRouter), fees);
+        messageId = senderRouter.ccipSend(destinationChainSelector, evm2AnyMessage);
+
+        return messageId;
+    }
+
+    function calculateRequestFees(
+        uint64 destinationChainSelector,
+        string calldata text
+    ) external view returns (uint256) {
+        require(senderRouter != IRouterClient(address(0)), "Sender not registered");
+
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(address(0)),
+            data: abi.encode(text),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 200_000, strict: false})
+            ),
+            feeToken: address(senderToken)
+        });
+
+        return senderRouter.getFee(destinationChainSelector, evm2AnyMessage);
+    }
+
+    bytes32 private lastReceivedMessageId;
+    string private lastReceivedText;
+
+    function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal {
+        lastReceivedMessageId = any2EvmMessage.messageId;
+        lastReceivedText = abi.decode(any2EvmMessage.data, (string));
     }
 }
