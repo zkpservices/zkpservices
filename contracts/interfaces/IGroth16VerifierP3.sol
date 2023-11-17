e58bc67ae49f0766560840e32b83d3122c5598bd
zkpservices <contact@zkp.services>
2023-11-18 09:51:46 +1100

    chore: 3 public inputs interface for Groth16Verifier

---
+++ contracts/interfaces/IGroth16VerifierP3.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.7;
+
+interface IGroth16VerifierP3 {
+    function verifyProof(
+        uint256[2] calldata _pA,
+        uint256[2][2] calldata _pB,
+        uint256[2] calldata _pC,
+        uint256[3] calldata _pubSignals
+    ) external view returns (bool);
+}
