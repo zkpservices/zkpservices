7f6c3d6131a81c7a449964bcb53d0f132723eda0
zkpservices <contact@zkp.services>
2023-11-19 18:53:29 +1100

    chore: created guides.md
    includes =  intro + make a request + make an update + cross-chain sync
    
    need to add = respond to a request (phil once modals are up) + 2FA (machin3boy at the end)

--- site/client/src/components/History.jsx
+++ site/client/src/components/History.jsx
@@ -14,31 +14,39 @@ export function History({ tableData = {}, showRefresh = true }) {
   return (
     <div className="xl:max-w-none mt-12 pb-8">
       <Tab.Group>
-        <div className="mb-8 space-x-1">
-          <Tab.List className="flex">
-            {tabs.map((tab) => (
-              <Tab
-                key={tab.name}
-                as="button"
-                className={({ selected }) =>
-                  classNames(
-                    selected
-                      ? 'bg-emerald-100 text-emerald-500 dark:bg-emerald-900 dark:text-emerald-400'
-                      : 'bg-gray-200 text-gray-500 hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700',
-                    'rounded-md px-3 py-2 text-sm font-medium mx-1'
-                  )
-                }
-              >
-                {tab.name}
-              </Tab>
-            ))}
-          </Tab.List>
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <Tab.List className="flex">
+              {tabs.map((tab) => (
+                <Tab
+                  key={tab.name}
+                  as="button"
+                  className={({ selected }) =>
+                    classNames(
+                      selected
+                        ? 'bg-emerald-100 text-emerald-500 dark:bg-emerald-900 dark:text-emerald-400'
+                        : 'bg-gray-200 text-gray-500 hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700',
+                      'rounded-md px-3 py-2 text-sm font-medium mx-1'
+                    )
+                  }
+                >
+                  {tab.name}
+                </Tab>
+              ))}
+            </Tab.List>
+            <button
+              className="px-3 py-2 text-sm mr-1.5 font-medium bg-emerald-100 text-emerald-500 hover:bg-emerald-200 dark:bg-emerald-900 dark:text-emerald-400 dark:hover:bg-emerald-800 rounded-md"
+              onClick={() => {}}
+            >
+              Refresh All
+            </button>
+          </div>
         </div>
         <div className="mt-4">
           <Tab.Panels className="w-full overflow-x-auto">
             {tabs.map((tab, index) => (
               <Tab.Panel key={index}>
-                {tableData[tab.name] && tableData[tab.name].length > 0 ? ( // Check if there are rows for the current tab
+                {tableData[tab.name] && tableData[tab.name].length > 0 ? (
                   <div className="p-1.5 min-w-full inline-block align-middle overflow-y-auto h-[400px]">
                     <div className={`border-2 transition ease-in-out duration-500 hover:border-emerald-400 hover:shadow-lg hover:ring-1 hover:ring-emerald-300 dark:hover:border-emerald-700 dark:hover:shadow-lg dark:hover:ring-1 dark:hover:ring-emerald-600 rounded-lg overflow-hidden dark:border-gray-700`}>
                       <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
@@ -67,7 +75,7 @@ export function History({ tableData = {}, showRefresh = true }) {
                           {tableData[tab.name].map((rowData, rowIndex) => (
                             <tr 
                               key={rowIndex}
-                              className="hover:bg-emerald-50 dark:hover:bg-emerald-800"  // Added this line for hover effect
+                              className="hover:bg-emerald-50 dark:hover:bg-emerald-800"
                             >
                               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
                                 {rowData.operation.map((op, i) => (
