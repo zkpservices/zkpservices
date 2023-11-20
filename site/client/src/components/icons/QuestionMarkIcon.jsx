720af988a730859e9b52ba85169b9159f3cee69b
zkpservices <contact@zkp.services>
2023-11-20 21:16:44 +1100

    feat: question mark icon

---
+++ site/client/src/components/icons/QuestionMarkIcon.jsx
@@ -0,0 +1,9 @@
+export function QuestionMarkIcon(props) {
+  return (
+    <svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
+      <text x="10" y="12" fontSize="20" textAnchor="middle" dominantBaseline="middle">
+        ?
+      </text>
+    </svg>
+  );
+}
