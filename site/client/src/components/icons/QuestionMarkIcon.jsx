27c5a5f4bcea09a5b88b77e3be21ae00619b5f79
zkpservices <contact@zkp.services>
2023-11-21 23:19:54 +1100

    chore: increase ? icon font

--- site/client/src/components/icons/QuestionMarkIcon.jsx
+++ site/client/src/components/icons/QuestionMarkIcon.jsx
@@ -1,7 +1,7 @@
 export function QuestionMarkIcon(props) {
   return (
     <svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
-      <text x="10" y="12" fontSize="20" textAnchor="middle" dominantBaseline="middle">
+      <text x="10" y="12" fontSize="23" textAnchor="middle" dominantBaseline="middle">
         ?
       </text>
     </svg>
