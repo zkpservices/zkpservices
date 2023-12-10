19e373a1d6b75c019c7cdbf8ea7841e13cd060e6
zkp.services <contact@zkp.services>
2023-12-11 05:20:09 +1100

    chore: CCIP fee correction - it's 5ZKP tokens not 10 now

--- site/client/src/components/NewCrossChainSyncModal.jsx
+++ site/client/src/components/NewCrossChainSyncModal.jsx
@@ -484,7 +484,7 @@ async function handleSubmit() {
                       className="font-mono relative mt-1 block w-full appearance-none rounded-md border border-gray-300 bg-slate-100 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-emerald-500 focus:outline-none focus:ring-emerald-500 dark:border-gray-600 dark:border-gray-700 dark:bg-slate-700 dark:text-white dark:placeholder-gray-300 dark:focus:border-emerald-500 sm:text-sm"
                       rows={1}
                       readOnly
-                      defaultValue="10 ZKP"
+                      defaultValue="5 ZKP"
                     />
                   </div>
 
